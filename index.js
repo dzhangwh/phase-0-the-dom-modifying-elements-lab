466959fefb26389c2e89a8adfb65aa63974421af
Daniel Zhang <dzhangwh@gmail.com>
2021-12-06 03:47:44 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader)
+
+newHeader.id = "victory";
+victory.innerHTML = "<h1>Daniel is the champion</h1>";
+
+
